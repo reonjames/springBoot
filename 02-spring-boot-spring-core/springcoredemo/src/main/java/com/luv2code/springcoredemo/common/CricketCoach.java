b8bda3ff04e2165f6cb35a02f109881a5b09e440
reonjames <reon.james.7@gmail.com>
2024-02-21 00:03:20 +0530

    bean class added

--- 02-spring-boot-spring-core/springcoredemo/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-spring-core/springcoredemo/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -1,11 +1,14 @@
-package com.luv2code.springcoredemo;
+package com.luv2code.springcoredemo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class CricketCoach implements Coach {
     @Override
     public String getDailyWorkout() {
+        System.out.println("In constructor"+getClass().getSimpleName());
         return "Practise fast bowling for 15 minutes";
     }
 }
